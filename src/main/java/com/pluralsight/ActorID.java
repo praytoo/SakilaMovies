7b7069bed692e8b4ed20ea9cd0bf28916f0599a2
praytoo <praytoo52@icloud.com>
2025-12-03 11:29:55 -0800

    update

--- src/main/java/com/pluralsight/ActorID.java
+++ src/main/java/com/pluralsight/ActorID.java
@@ -1,8 +1,23 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.util.List;
 
 public class ActorID {
+    public static void main(String[] args) {
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername("root");
+        dataSource.setPassword("yearup");
+
+        SakilaMoviesDataManager dataManager = new SakilaMoviesDataManager(dataSource);
+
+        List<ActorID> actorId = dataManager.getActorID();
+
+        actorId.forEach(System.out::println);
+    }
     private int film_id;
     private String title;
     private String description;
